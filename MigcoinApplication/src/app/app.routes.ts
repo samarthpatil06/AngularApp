be8c066dfb1677420ba5585f94c7d509387fcd99
samarthpatil06 <samarthpatil2744@gmail.com>
2026-01-12 10:39:58 +0530

    remove commented code

--- MigcoinApplication/src/app/app.routes.ts
+++ MigcoinApplication/src/app/app.routes.ts
@@ -1,23 +1,3 @@
-// import { Routes } from '@angular/router';
-
-// export const routes: Routes = [
-//     {
-//     path: '',
-//     loadComponent: () =>
-//       import('./login/login.component').then(m => m.LoginComponent)
-//   },
-//   {
-//     path: 'dashboard',
-//     loadComponent: () =>
-//       import('./MainLayout/components/dashboard/dashboard.component').then(m => m.DashboardComponent)
-//   },
-//   {
-//     path: 'view',
-//     loadComponent: () =>
-//       import('./MainLayout/components/view/view.component').then(m => m.ViewComponent)
-//   }
-    
-// ];
 
 
 import { Routes } from '@angular/router';
