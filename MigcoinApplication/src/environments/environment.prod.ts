c7e1fe07aa3c462bd5dcd4c89a1e1e12e26cd2fc
siddharth116-sudo <magdumsiddharth111@gmail.com>
2026-02-04 03:01:36 +0530

    Ready for AWS deployment

--- MigcoinApplication/src/environments/environment.prod.ts
+++ MigcoinApplication/src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
+// This file can be replaced during build by using the `fileReplacements` array when uploading to AWS. when uploading to AWS I used 
+// below environment file to point to the correct API URL.
+
+// export const environment = {
+//   production: true,
+//   apiUrl: 'http://localhost:3000/api' // Note: In Docker, this usually stays localhost:3000 because the BROWSER is accessing it.
+// };
+
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api' // Note: In Docker, this usually stays localhost:3000 because the BROWSER is accessing it.
+  apiUrl: '/api'  // Note: No http://, no IP, no Port! 
 };
