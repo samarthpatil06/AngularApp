f7b29249eb6f9d12d5739d18dc96504de3b445b1
siddharth116-sudo <magdumsiddharth111@gmail.com>
2026-02-07 07:35:58 +0530

    view added by samarth

--- MigcoinApplication/src/app/MainLayout/components/view/view.component.ts
+++ MigcoinApplication/src/app/MainLayout/components/view/view.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardService } from '../../../services/dashboard.service';
 
 @Component({
   selector: 'app-view',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './view.component.html',
-  styleUrl: './view.component.scss'
+  styleUrl: './view.component.scss',
 })
 export class ViewComponent {
+  totalUsers: number | null = null;
+  data: any;
 
+  constructor(private dashboardService: DashboardService) {}
+
+  ngOnInit() {
+    console.log('Component loaded');
+
+    this.dashboardService.getSummary().subscribe((res) => {
+      this.data = res;
+    });
+  }
 }
