bf460ecb0b06ed42f79a3c18409e6bd7f9fdc688
siddharth116-sudo <magdumsiddharth111@gmail.com>
2026-02-09 11:34:19 +0530

    chagned dashboard.service.ts -> imported environment and change getsummary

--- MigcoinApplication/src/app/services/dashboard.service.ts
+++ MigcoinApplication/src/app/services/dashboard.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
@@ -9,6 +10,6 @@ export class DashboardService {
     constructor(private http: HttpClient) { }
 
     getSummary() {
-        return this.http.get<any>('http://localhost:3000/api/dashboard/summary');
+        return this.http.get<any>(`${environment.apiUrl}/dashboard/summary`);
     }
 }
