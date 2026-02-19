8ac8774b55a48bf6a5001db3f8af6672a4e09fb8
siddharth116-sudo <magdumsiddharth111@gmail.com>
2026-02-19 12:17:44 +0530

    All done for Mobile

--- MigcoinApplication/src/environments/environment.prod.ts
+++ MigcoinApplication/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: 'http://192.168.56.1:3000/api'  // Note: No http://, no IP, no Port! 
+  production: false,
+  apiUrl: `http://${window.location.hostname}:3000/api`
 };
